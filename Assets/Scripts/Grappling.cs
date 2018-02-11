1890d685e93335f91e9c91e39a4ed8eb06ad7072
Nin Azurifle <dsirikasem@hotmail.com>
2018-02-11 15:48:57 +0700

    First Github setup
    Razor Hydra: Pick & Throw

---
+++ Assets/Scripts/Grappling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class Grappling : MonoBehaviour {
+    
+    internal Vector3 GetPosition(SixenseHands hand)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal Vector3 GetRotation(SixenseHands hand)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+/*not being use yet
+    
+    bool isGrabbedByLeft = false, isGrabbedByRight = false;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (isGrabbedByLeft || isGrabbedByRight)
+            return;
+
+        if (collision.gameObject.CompareTag("Hand"))
+        {
+            if (SixenseInput.Controllers[0].GetButton(SixenseButtons.TRIGGER))
+            {
+                Debug.Log("grab by left");
+                isGrabbedByLeft = true;
+                transform.SetParent(collision.transform);
+            }
+            else if (SixenseInput.Controllers[1].GetButton(SixenseButtons.TRIGGER))
+            {
+                Debug.Log("grab by right");
+                isGrabbedByRight = true;
+                transform.SetParent(collision.transform);
+            }
+        }
+    }*/
---
+++ Assets/Sixense Assets/SixenseInput/DemoScenes/Scripts/SixenseHand.cs
@@ -0,0 +1,228 @@
+
+//
+// Copyright (C) 2013 Sixense Entertainment Inc.
+// All Rights Reserved
+//
+
+using UnityEngine;
+
+public class SixenseHand : SixenseObjectController
+{
+	//public SixenseHands Hand;//m_hand
+    public SixenseInput.Controller m_controller = null;
+
+	Animator 	m_animator;
+	float 		m_fLastTriggerVal;
+	//Vector3		m_initialPosition;
+	//Quaternion 	m_initialRotation;
+    
+    protected override void Start() 
+	{
+		// get the Animator
+		m_animator = gameObject.GetComponent<Animator>();
+		m_initialRotation = transform.localRotation;
+		m_initialPosition = transform.localPosition;
+        
+        grabPoint = transform.GetChild(0);
+        /*currentPosition = GameObject.Find("RightHandCollider").transform.position;
+        currentRotation = GameObject.Find("RightHandCollider").transform.rotation;*/
+    }
+
+    protected override void Update()//"override" was added by Dandy
+    {
+		if ( m_controller == null )
+		{
+			m_controller = SixenseInput.GetController( Hand );
+		}
+		else if ( m_animator != null )
+		{
+            //UpdateHandAnimation();
+            UpdateObject(m_controller);//was added by Dandy
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (m_controller != null && m_controller.Enabled)
+            UpdateActionInput(m_controller); //Action update
+    }
+
+    // Updates the animated object from controller input.
+    protected void UpdateHandAnimation()
+	{
+		// Point
+		if ( Hand == SixenseHands.RIGHT ? m_controller.GetButton(SixenseButtons.ONE) : m_controller.GetButton(SixenseButtons.TWO) )
+		{
+			m_animator.SetBool( "Point", true );
+		}
+		else
+		{
+			m_animator.SetBool( "Point", false );
+		}
+		
+		// Grip Ball
+		if ( Hand == SixenseHands.RIGHT ? m_controller.GetButton(SixenseButtons.TWO) : m_controller.GetButton(SixenseButtons.ONE)  )
+		{
+			m_animator.SetBool( "GripBall", true );
+		}
+		else
+		{
+			m_animator.SetBool( "GripBall", false );
+		}
+				
+		// Hold Book
+		if ( Hand == SixenseHands.RIGHT ? m_controller.GetButton(SixenseButtons.THREE) : m_controller.GetButton(SixenseButtons.FOUR) )
+		{
+			m_animator.SetBool( "HoldBook", true );
+		}
+		else
+		{
+			m_animator.SetBool( "HoldBook", false );
+		}
+				
+		// Fist
+		float fTriggerVal = m_controller.Trigger;
+		fTriggerVal = Mathf.Lerp( m_fLastTriggerVal, fTriggerVal, 0.1f );
+		m_fLastTriggerVal = fTriggerVal;
+		
+		if ( fTriggerVal > 0.01f )
+		{
+			m_animator.SetBool( "Fist", true );
+		}
+		else
+		{
+			m_animator.SetBool( "Fist", false );
+		}
+		
+		m_animator.SetFloat("FistAmount", fTriggerVal);
+		
+		// Idle
+		if ( m_animator.GetBool("Fist") == false &&  
+			 m_animator.GetBool("HoldBook") == false && 
+			 m_animator.GetBool("GripBall") == false && 
+			 m_animator.GetBool("Point") == false )
+		{
+			m_animator.SetBool("Idle", true);
+		}
+		else
+		{
+			m_animator.SetBool("Idle", false);
+		}
+	}
+
+	public Quaternion InitialRotation
+	{
+		get { return m_initialRotation; }
+	}
+	
+	public Vector3 InitialPosition
+	{
+		get { return m_initialPosition; }
+	}
+
+    /* Razer Hydra Tutorial____________________________________
+    |
+    | https://www.slideshare.net/cjros/basic-vr-development-tutorial-integrating-oculus-rift-and-razer-hydra
+    |__________________________________________________________*/
+
+    public float minGrabDistance = 0.002f, throwForce = 3.0f;
+    // Grabbable object must be within this distance from hand colliders to be picked up 
+    // Force multiplyer for throwing objects
+    public Transform itemsParent;
+
+    private bool isHoldingObject = false;
+    private Transform grabPoint;
+    private GameObject closestObject = null;
+    private Rigidbody closestRigidbody = null;
+    private Grappling closestGrappling = null;
+    // Script attached to grabbed object with grappling data on that object 
+    //private float handVelocity = 0.0f;
+    private Vector3 handVector, handPrevious;
+
+    protected override void UpdateObject(SixenseInput.Controller controller)
+    {
+        if (controller.Enabled)
+        {
+            //UpdateAnimationInput(controller); //Animation update
+            UpdateHandAnimation();
+            //Action update move to fixedUpdate by Dandy
+        }
+
+        base.UpdateObject(controller);
+    }
+
+    protected void UpdateActionInput(SixenseInput.Controller controller)
+    {
+        if (isHoldingObject)
+        {
+            FindVelocity();
+
+            if (!controller.GetButton(SixenseButtons.TRIGGER))
+            {
+                closestObject.transform.SetParent(itemsParent);
+                closestRigidbody.isKinematic = false;
+                Throw();
+                isHoldingObject = false;
+                closestObject = null;
+                return;
+            }
+        }
+        else if(controller.GetButton(SixenseButtons.TRIGGER) )
+        {
+            GameObject[] grabbables = GameObject.FindGameObjectsWithTag("Grabbable");
+            float dist
+                , nearest = Vector3.Distance(grabbables[0].transform.position, grabPoint.position);
+            if (nearest < minGrabDistance)
+                closestObject = grabbables[0];
+
+            for (int i = 1; i < grabbables.Length; ++i)
+            {
+                dist = Vector3.Distance(grabbables[i].transform.position, grabPoint.position);
+                if (dist < minGrabDistance && dist < nearest)
+                {
+                    closestObject = grabbables[i];
+                    nearest = dist;
+                }
+            }
+
+            if (!closestObject)
+                return;
+
+            closestRigidbody = closestObject.GetComponent<Rigidbody>();
+            if (closestRigidbody && closestRigidbody.isKinematic)
+                return;
+
+            closestRigidbody.isKinematic = true;
+
+            closestObject.transform.position = grabPoint.position;
+            closestObject.transform.rotation = grabPoint.rotation;
+            closestObject.transform.SetParent(grabPoint);
+
+            isHoldingObject = true;
+        }
+    }
+
+    //Calculate velocity of hand 
+    protected void FindVelocity()
+    {
+        if (Time.fixedDeltaTime != 0)//Time.deltaTime
+        {
+            handVector = (transform.position - handPrevious) / Time.fixedDeltaTime;//Time.deltaTime
+            handPrevious = transform.position;
+        }
+
+        //handVelocity = Vector3.Magnitude(handVector);
+    }
+
+    //Throw the held object once player lets go based on hand velocity 
+    protected void Throw()
+    {
+        if (closestRigidbody)
+        {
+            //Vector3 dir = (closestObject.transform.position - transform.position).normalized;
+            closestRigidbody.AddForce(/*dir * handVelocity*/ handVector * throwForce);
+        }
+    }
+
+}//class
+
