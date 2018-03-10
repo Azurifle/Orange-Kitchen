d2c4cd11d7732d9a50fb9b9473d3162244f98b17
sapphireVGC <siribhums8534@gmail.com>
2018-03-10 21:18:34 +0700

    Mock up

---
+++ Assets/3. Scripts/HeaterScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeaterScript : MonoBehaviour {
+
+    public GameObject fire;
+
+    private Color alpha;
+    private bool isHeat = false;
+    private bool isOn = false;
+
+	// Use this for initialization
+	void Start () {
+        alpha = fire.GetComponent<MeshRenderer>().materials[0].color;
+        alpha.a = 0;
+        fire.GetComponent<MeshRenderer>().materials[0].color = alpha;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isOn)
+        {
+            if (alpha.a < 255)
+                alpha.a += 0.01f;
+            else
+                isHeat = true;
+        }
+        else
+        {
+            if (alpha.a > 0)
+                alpha.a -= 0.01f;
+            else
+                isHeat = false;
+        }
+
+        fire.GetComponent<MeshRenderer>().materials[0].color = alpha;
+    }
+
+    private void OnMouseDown()
+    {
+        isOn = !isOn;
+    }
+}
---
+++ Assets/3. Scripts/RefrigeratorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefrigeratorScript : MonoBehaviour {
+
+    public Animator anim;
+
+    private bool onSwitch = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && onSwitch)
+            anim.SetTrigger("Push");
+	}
+
+    private void OnMouseEnter()
+    {
+        onSwitch = true;
+    }
+
+    private void OnMouseExit()
+    {
+        onSwitch = false;
+    }
+}
