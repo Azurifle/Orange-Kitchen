3b8f864f9118c1cf2708386e430289d9e7dd5cd5
sapphireVGC <siribhums8534@gmail.com>
2018-03-27 18:53:22 +0700

    test order

---
+++ Assets/3. Scripts/FoodCheckerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCheckerScript : MonoBehaviour
+{
+    [HideInInspector] public bool isDeliver = true;
+
+    [HideInInspector] public int seatNo;
+
+    [HideInInspector] public int noodleReq;
+    [HideInInspector] public int porkReq;
+    [HideInInspector] public int soupReq;
+    [HideInInspector] public int wontonReq;
+    [HideInInspector] public int veggyReq;
+
+    [HideInInspector] public int noodleCnt = 0;
+    [HideInInspector] public int porkCnt = 0;
+    [HideInInspector] public int soupCnt = 0;
+    [HideInInspector] public int wontonCnt = 0;
+    [HideInInspector] public int veggyCnt = 0;
+
+    [HideInInspector] public GameObject bowl;
+
+    public GameObject text;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDeliver)
+        {
+            text.GetComponent<TextMesh>().text = "";
+        }
+        else
+        {
+            string t;
+
+            if (soupReq == 1) t = "Yes";
+            else t = "No";
+
+            text.GetComponent<TextMesh>().text = "Table: " + seatNo.ToString() + 
+                                                "\nNoodle: " + noodleReq.ToString() + 
+                                                "\nPork: " + porkReq.ToString() +
+                                                "\nSoup: " + t;
+        }
+    }
+}
+
---
+++ Assets/3. Scripts/WhiteboardScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteboardScript : MonoBehaviour {
+
+    public GameObject[] notes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeOrder ()
+    {
+        if (notes[0].GetComponent<FoodCheckerScript>().isDeliver)
+        {
+            notes[0].GetComponent<FoodCheckerScript>().isDeliver = false;
+            notes[0].GetComponent<FoodCheckerScript>().seatNo = 1;
+            notes[0].GetComponent<FoodCheckerScript>().noodleReq = 2;
+            notes[0].GetComponent<FoodCheckerScript>().porkReq = 5;
+            notes[0].GetComponent<FoodCheckerScript>().soupReq = 1;
+        }
+        else if (notes[1].GetComponent<FoodCheckerScript>().isDeliver)
+        {
+            notes[1].GetComponent<FoodCheckerScript>().isDeliver = false;
+            notes[1].GetComponent<FoodCheckerScript>().seatNo = 2;
+        }
+        else if (notes[2].GetComponent<FoodCheckerScript>().isDeliver)
+        {
+            notes[2].GetComponent<FoodCheckerScript>().isDeliver = false;
+            notes[2].GetComponent<FoodCheckerScript>().seatNo = 3;
+        }
+        else if (notes[3].GetComponent<FoodCheckerScript>().isDeliver)
+        {
+            notes[3].GetComponent<FoodCheckerScript>().isDeliver = false;
+            notes[3].GetComponent<FoodCheckerScript>().seatNo = 4;
+        }
+    }
+}
