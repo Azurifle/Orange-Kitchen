5dd2266e137bec587eb85195cac721a545dcd94f
sapphireVGC <siribhums8534@gmail.com>
2018-04-18 19:10:16 +0700

    CUSTOMER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Assets/3. Scripts/CustomerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerScript : MonoBehaviour {
+
+    public GameObject Door;
+    public GameObject SpwnMark;
+    public GameObject[] SeatMark;
+
+    public bool RealOne;
+
+    private Animator doorAnim;
+
+    private SpawnScript spwn;
+
+    private bool atSeat = false;
+
+    private int target;
+
+    // Use this for initialization
+    void Start () {
+        doorAnim = Door.GetComponent<Animator>();
+        spwn = SpwnMark.GetComponent<SpawnScript>();
+        target = SpawnScript.chairNo;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!RealOne)
+        {
+            if (Vector3.Distance(transform.position, SeatMark[target].transform.position) <= 4.0f) atSeat = true;
+
+            if (!atSeat)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, SeatMark[target].transform.position, 10 * Time.deltaTime);
+            }
+            else if (atSeat && Vector3.Distance(transform.position, spwn.Target[target].transform.position) >= 0.5f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, spwn.Target[target].transform.position, 10 * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/3. Scripts/SpawnScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public static int chairNo = -1;
+
+    public static bool[] free = new bool[4];
+
+    public GameObject SpwnMark;
+    public GameObject Customer;
+    public GameObject Door;
+
+    public Transform[] Target;
+
+    private Animator doorAnim;
+
+	// Use this for initialization
+	void Start () {
+        doorAnim = Door.GetComponent<Animator>();
+
+        for (int i = 3; i >= 0; i--)
+            free[i] = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateCustomer ()
+    {
+        bool seatFree = false;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (free[i])
+            {
+                chairNo = i;
+                free[i] = false;
+                seatFree = true;
+                
+                break;
+            }
+        }
+
+        if (seatFree)
+        {
+            Vector3 point = SpwnMark.transform.position;
+
+            point.y += 5;
+
+            GameObject newOne = Instantiate(Customer, point, Quaternion.identity);
+
+            newOne.GetComponent<CustomerScript>().RealOne = false;
+
+            StartCoroutine(DoorActivate());
+        }
+    }
+
+    IEnumerator DoorActivate()
+    {
+        doorAnim.SetBool("IsOpen", true);
+
+        yield return new WaitForSeconds(2.0f);
+
+        doorAnim.SetBool("IsOpen", false);
+    }
+}
