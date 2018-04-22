ac35d8bc34624a95110a8592eb19e17eb08029f9
sapphireVGC <siribhums8534@gmail.com>
2018-04-22 13:03:38 +0700

    START SCENEEEEEEEE !!!!!!!!!!!!!!!!!!!

---
+++ Assets/3. Scripts/MenuCameraScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraScript : MonoBehaviour {
+
+    public GameObject[] CameraMark;
+    public GameObject CameraLookAt;
+
+    private int node = 0;
+
+    private bool plus = true;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = CameraMark[0].transform.position;
+        transform.LookAt(CameraLookAt.transform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(transform.position, CameraMark[node].transform.position) <= 0.5f)
+        {
+            if (plus)
+                node++;
+            else
+                node--;
+        }
+
+        if (node > 2)
+        {
+            node = 1;
+            plus = false;
+        }
+        else if (node < 0)
+        {
+            node = 1;
+            plus = true;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, CameraMark[node].transform.position, 3 * Time.deltaTime);
+        transform.LookAt(CameraLookAt.transform);
+    }
+}
---
+++ Assets/3. Scripts/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    public Text StartText;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Blink());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Blink ()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+
+            StartText.gameObject.SetActive(false);
+
+            yield return new WaitForSeconds(0.5f);
+
+            StartText.gameObject.SetActive(true);
+        }
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+}
